b88781c72912eb0ebbccc931b88de48d4094c5e4
Alayna Truttmann <atruttmann@wisc.edu>
2017-09-11 00:23:17 -0500

    Moved scripts from index.html to separate JS file, fixed IE modal issues, tweaked title animation

---
+++ js/script.js
@@ -0,0 +1,85 @@
+// Modal fix for Internet Explorer
+$(function () {
+    var isIE = window.ActiveXObject || "ActiveXObject" in window;
+    if (isIE) {
+        $('.modal').removeClass('fade');
+    }
+});
+
+// PlanIt Captions
+$(function() {
+    $('.carousel').carousel();
+    var caption = $('div.item:nth-child(1) .caption-planit');
+    $('#caption-planit span').html(caption.html());
+    caption.css('display','none');
+    $(".carousel").on('slide.bs.carousel', function(evt) {
+        var caption = $('div.item:nth-child(' + ($(evt.relatedTarget).index()+1) + ') .caption-planit');
+        $('#caption-planit span').html(caption.html());
+        caption.css('display','none');
+    });
+});
+
+// WACM Captions
+$(function() {
+    $('.carousel').carousel();
+    var caption = $('div.item:nth-child(1) .caption-wacm');
+    $('#caption-wacm span').html(caption.html());
+    caption.css('display','none');
+    $(".carousel").on('slide.bs.carousel', function(evt) {
+        var caption = $('div.item:nth-child(' + ($(evt.relatedTarget).index()+1) + ') .caption-wacm');
+        $('#caption-wacm span').html(caption.html());
+        caption.css('display','none');
+    });
+});
+
+// Draw3D Captions
+$(function() {
+    $('.carousel').carousel();
+    var caption = $('div.item:nth-child(1) .caption-draw3D');
+    $('#caption-draw3D span').html(caption.html());
+    caption.css('display','none');
+    $(".carousel").on('slide.bs.carousel', function(evt) {
+        var caption = $('div.item:nth-child(' + ($(evt.relatedTarget).index()+1) + ') .caption-draw3D');
+        $('#caption-draw3D span').html(caption.html());
+        caption.css('display','none');
+    });
+});
+
+// Photo Booth Captions
+$(function() {
+    $('.carousel').carousel();
+    var caption = $('div.item:nth-child(1) .caption-photobooth');
+    $('#caption-photobooth span').html(caption.html());
+    caption.css('display','none');
+    $(".carousel").on('slide.bs.carousel', function(evt) {
+        var caption = $('div.item:nth-child(' + ($(evt.relatedTarget).index()+1) + ') .caption-photobooth');
+        $('#caption-photobooth span').html(caption.html());
+        caption.css('display','none');
+    });
+});
+
+// Intuit Captions
+$(function() {
+    $('.carousel').carousel();
+    var caption = $('div.item:nth-child(1) .caption-intuit');
+    $('#caption-intuit span').html(caption.html());
+    caption.css('display','none');
+    $(".carousel").on('slide.bs.carousel', function(evt) {
+        var caption = $('div.item:nth-child(' + ($(evt.relatedTarget).index()+1) + ') .caption-intuit');
+        $('#caption-intuit span').html(caption.html());
+        caption.css('display','none');
+    });
+});
+
+// Microsoft Captions
+$(function() {
+    $('.carousel').carousel();
+    var caption = $('div.item:nth-child(1) .caption-microsoft');
+    $('#caption-microsoft span').html(caption.html());
+    caption.css('display','none');
+    $(".carousel").on('slide.bs.carousel', function(evt) {
+        var caption = $('div.item:nth-child(' + ($(evt.relatedTarget).index()+1) + ') .caption-microsoft');
+        $('#caption-microsoft span').html(caption.html());
+        caption.css('display','none');
+    });
+});
