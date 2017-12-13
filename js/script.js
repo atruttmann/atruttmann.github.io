2fd7bf8b4175198cf99dffaa967749832f9fcad9
Alayna Truttmann <atruttmann@wisc.edu>
2017-12-13 17:49:42 -0600

    Added Calf Tracker project

--- js/script.js
+++ js/script.js
@@ -7,6 +7,19 @@ $(function () {
     }
 });
 
+// Calf Tracker Captions
+$(function() {
+    $('.carousel').carousel();
+    var caption = $('div.item:nth-child(1) .caption-calftracker');
+    $('#caption-calftracker span').html(caption.html());
+    caption.css('display','none');
+    $(".carousel").on('slide.bs.carousel', function(evt) {
+        var caption = $('div.item:nth-child(' + ($(evt.relatedTarget).index()+1) + ') .caption-calftracker');
+        $('#caption-calftracker span').html(caption.html());
+        caption.css('display','none');
+    });
+});
+
 // PlanIt Captions
 $(function() {
     $('.carousel').carousel();
