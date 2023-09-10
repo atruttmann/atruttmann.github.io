2559570bbd4b3cc55f55d81efdb3480d22765461
Alayna Truttmann <amtruttmann@gmail.com>
2023-09-09 19:44:35 -0700

    Add new resume

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -16,10 +16,7 @@ function Header() {
         skills and eye for design to create great user experiences.
       </p>
       <div className="linkIcons">
-        <a
-          href="https://www.linkedin.com/in/alaynatruttmann/"
-          aria-label="LinkedIn"
-        >
+        <a href="https://www.linkedin.com/in/atruttmann/" aria-label="LinkedIn">
           <FaLinkedin />
         </a>
         <a href="download/Resume-Alayna-Truttmann.pdf" aria-label="Resume">
