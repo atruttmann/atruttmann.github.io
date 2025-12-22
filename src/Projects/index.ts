657e2efad6e2503afe73d0e4c1680b410bb9d1e7
Alayna Truttmann <amtruttmann@gmail.com>
2025-12-22 09:50:23 -0800

    Rename folders

--- src/Projects/index.ts
+++ src/Projects/index.ts
@@ -1,13 +1,13 @@
-import type { ProjectData } from "@types";
-import Pyro from "./Pyro";
-import Toolkit from "./Toolkit";
-import Flow from "./Flow";
-import Puffin from "./Puffin";
-import Oso from "./Roku_Oso";
-import UXE from "./Roku_UXE";
-import NBCUX from "./NBCUX";
-import MobilePlayer from "./Peacock_MobilePlayer";
-import MobileInnovation from "./Peacock_MobileInnovation";
+import type { ProjectData } from '@types';
+import Pyro from './Intuit_Pyro';
+import Toolkit from './Intuit_Toolkit';
+import Flow from './Roku_Flow';
+import Puffin from './Roku_Puffin';
+import Oso from './Roku_Oso';
+import UXE from './Roku_UXE';
+import NBCUX from './NBCUX';
+import MobilePlayer from './Peacock_MobilePlayer';
+import MobileInnovation from './Peacock_MobileInnovation';
 
 const ProjectsList: ProjectData[] = [
   MobilePlayer,
